e76ae8378c8d96e464cae14f036337d17fc1267c
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-06 17:52:29 +0100

    Adds navlinks into the header to navigate to the projects and about components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 import Hello from "./Hello"
 import Header from "./Header"
@@ -15,7 +15,11 @@ const App = () => {
             <div className="flex flex-col w-full fixed pin-l pin-y">
                 <Header />
                 <div className="px-4 pt-4 flex-1 overflow-y-scrol">
-                    <Hello />
+                    <Switch>
+                        <Route exact path='/' component={Hello}></Route>
+                        <Route exact path='/about' component={About}></Route>
+                        <Route exact path='/projects' component={Projects}></Route>
+                    </Switch>
                 </div>
                 <Footer />
             </div >
