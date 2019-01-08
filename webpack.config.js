5a0edf362f2f7253a41c9b538898ae7da5123769
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-08 12:29:16 +0100

    Adds code to webpack to fix the routing issue on rerender, adds github and website buttons with placeholder links and some styling

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,15 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
-    watchContentBase: true
+    watchContentBase: true,
+    historyApiFallback: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 }
