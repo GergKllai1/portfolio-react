a199007853107d4f178d7e15df7ee16890a2357d
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-08 13:01:48 +0100

    Implements basic project detail popup

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import Popup from 'reactjs-popup'
+import Popup from "reactjs-popup";
 
 const ProjectCard = props => {
   let project = props.project;
+  let Card = ({ title }) => (
+    <div className="card">
+      <div className="header">
+        {title}
+        <div className="content">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam illum
+          minus alias, corporis debitis odio explicabo nihil? Repellendus
+          itaque, et minus magni nihil maxime dolores esse dolorum eaque quas
+          id!
+        </div>
+      </div>
+    </div>
+  );
   return (
     <div key={project.id} className="overflow-hidden border">
       <img
@@ -17,12 +30,20 @@ const ProjectCard = props => {
       </div>
 
       <div className="flex items-center justify-around leading-none p-2 md:p-4">
-        <img
-            src="../src/images/details.png"
-            alt="Oops there should be an image here!"
-            className="p-px border-2 h-12 rounded-full rounded-lg hover:border-black"
-          />
-            
+        <Popup
+          trigger={
+            <img
+              src="../src/images/details.png"
+              alt="Oops there should be an image here!"
+              className="p-px border-2 h-12 rounded-full rounded-lg hover:border-black"
+            />
+          }
+          position="top center"
+          on="click"
+        >
+          <Card title={project.name} />
+        </Popup>
+
         <a href={project.github} target="_blank">
           <img
             src="../src/images/github2.png"
@@ -30,15 +51,17 @@ const ProjectCard = props => {
             className="p-px border-2 h-12 rounded-full rounded-lg hover:border-black"
           />
         </a>
-        {project.website ? 
-        <a href={project.website} target="_blank">
-          <img
-            src="../src/images/website.png"
-            alt="Oops there should be an image here!"
-            className="p-px border-2 h-12 rounded-full rounded-lg hover:border-black"
-          />
-        </a>
-        : ''}
+        {project.website ? (
+          <a href={project.website} target="_blank">
+            <img
+              src="../src/images/website.png"
+              alt="Oops there should be an image here!"
+              className="p-px border-2 h-12 rounded-full rounded-lg hover:border-black"
+            />
+          </a>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
