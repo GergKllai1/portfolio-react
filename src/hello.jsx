0b8a0ca8a545fe71a53be0ddfb9743b29de44c15
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-09 08:04:22 +0100

    Adds projects

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,18 +1,10 @@
 import React from "react"
-import { Parallax, Background } from 'react-parallax';
+import './Hello.css'
 
 
 const Hello = () => {
     return (
-        <Parallax
-        blur={{ min: -15, max: 15 }}
-        bgImage={require('../src/images/main.jpeg')}
-        bgImageAlt="the dog"
-        strength={-200}
-    >
-        Blur transition from min to max
-        <div style={{ height: '200px' }} />
-    </Parallax>
+        <h1 class="random">Hello World</h1>
     )
 }
 
